3a0392b03022cc37b5a8babce6d479322d0fce5c
Romain <163325746+elvirol@users.noreply.github.com>
2024-07-24 21:10:37 +0200

    Add files via upload

---
+++ exercice_js_scroll_pop-up/script.js
@@ -0,0 +1,37 @@
+// Créer un événement au scroll
+
+// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+
+// Faire apparaitre l'image de la partie improvise
+
+// Faire apparaitre la popup quand on est en bas du site
+
+// Bonus : quand on clicke sur la popup elle disparait pour toujours
+let lastScroll = 0;
+let playOnce = true;
+
+window.addEventListener("scroll", () => {
+  if (scrollY > lastScroll) {
+    navbar.style.height = 50 + "px";
+    lastScroll = scrollY;
+  } else {
+    navbar.style.height = 90 + "px";
+    lastScroll = scrollY;
+  }
+  let scrollValue =
+    (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+  if (scrollValue > 0.45) {
+    imgImprovise.style.opacity = 1;
+    imgImprovise.style.transform = "none";
+  }
+  if (scrollValue > 0.85 && playOnce) {
+    popup.style.opacity = 1;
+    popup.style.transform = "none";
+    playOnce = false;
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.style.opacity = 0;
+  popup.style.transform = "translate(500px)";
+});
